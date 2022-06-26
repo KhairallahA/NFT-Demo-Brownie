161e6103ed7a078c2878c5672dd6fe49e9114045
KhairallahA <khairallahfreelancer@gmail.com>
2022-06-26 22:44:03 +0300

    Deploy and Create Simple Collectible

---
+++ tests/test_simple_collectible.py
@@ -0,0 +1,13 @@
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
+from brownie import network
+from scripts.deploy_and_create import deploy_and_create
+import pytest
+
+
+def test_can_create_simple_collectible():
+    # Arrange / Act
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    simple_collectible = deploy_and_create()
+    # Assert
+    assert simple_collectible.ownerOf(0) == get_account()
